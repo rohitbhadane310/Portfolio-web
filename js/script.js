6c756a8fcac90a10e2b16e6bb1d18b7544279525
Rohit Bhadane <rohitbhadane310@gmail.com>
2024-08-01 20:41:23 +0530

    portfolio-web

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Designer",
+    "Youtuber",
+    "Sql Developer",
+    "Javascript Developer",
+    "Web Developer",
+    "Java programmer",
+    "React Developer",
+  ],
+  typeSpeed: 100,
+  BackSpeed: 50,
+  loop: true,
+});
